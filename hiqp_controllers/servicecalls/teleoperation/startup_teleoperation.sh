57a7861251607d8a4990a208b9901468fb157f2e
neckutrek <marcus.adam.johansson@gmail.com>
2016-11-25 15:25:33 +0100

    Fixed teleoperation

---
+++ hiqp_controllers/servicecalls/teleoperation/startup_teleoperation.sh
@@ -0,0 +1,33 @@
+rosservice call /yumi/hiqp_kinematics_controller/add_primitive \
+"name: 'teleop_point'
+type: 'point'
+frame_id: 'yumi_body'
+visible: true
+color: [0.0, 0.0, 1.0, 0.9]
+parameters: [0.4, 0, 0]"
+
+rosservice call /yumi/hiqp_kinematics_controller/add_primitive \
+"name: 'teleop_sphere'
+type: 'sphere'
+frame_id: 'yumi_body'
+visible: true
+color: [1.0, 0.0, 0.0, 0.75]
+parameters: [0, 0, 0.1, 0.03]"
+
+rosservice call /yumi/hiqp_kinematics_controller/add_primitive \
+"name: 'teleop_point2'
+type: 'point'
+frame_id: 'gripper_r_base'
+visible: true
+color: [0.0, 1.0, 0.0, 0.9]
+parameters: [0, 0, 0.1]"
+
+sleep 1.0
+
+rosservice call /yumi/hiqp_kinematics_controller/set_task \
+"name: 'task_teleop'
+priority: 2
+visible: 1
+active: 1
+def_params: ['TDefGeomProj', 'point', 'point', 'teleop_point = teleop_point2']
+dyn_params: ['TDynFirstOrder', '1.0']"
