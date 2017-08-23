432d80ec2033fdd98a985529bde7586615465c07
Yumi <yumi@oru>
2017-08-23 18:08:27 +0200

    changed alignments to cross product

--- hiqp_core/src/tasks/tdef_geometric_alignment.cpp
+++ hiqp_core/src/tasks/tdef_geometric_alignment.cpp
@@ -40,35 +40,35 @@ namespace hiqp {
       KDL::Vector v2 = pose_b_.M * line2->getDirectionKDL();
 
       //DOT PRODUCT =============================================      
-      return alignUnitVectors(v1, v2, robot_state);
+      //return alignUnitVectors(v1, v2, robot_state);
       //DOT PRODUCT END =========================================
 
       //CROSS PRODUCT =============================================      
-      // double q_nr=jacobian_a_.columns();
-      // KDL::Jacobian J_v1, J_p1, J_v2, J_v1_dot, J_v2_dot;
-      // J_v1.resize(q_nr);
-      // J_v2.resize(q_nr);
-      // J_p1.resize(q_nr);
-      // J_v1_dot.resize(q_nr);
-      // J_v2_dot.resize(q_nr);
+      double q_nr=jacobian_a_.columns();
+      KDL::Jacobian J_v1, J_p1, J_v2, J_v1_dot, J_v2_dot;
+      J_v1.resize(q_nr);
+      J_v2.resize(q_nr);
+      J_p1.resize(q_nr);
+      J_v1_dot.resize(q_nr);
+      J_v2_dot.resize(q_nr);
     
-      // changeJacRefPoint(jacobian_a_, v1, J_v1);
-      // J_v1.data=J_v1.data - jacobian_a_.data;
+      changeJacRefPoint(jacobian_a_, v1, J_v1);
+      J_v1.data=J_v1.data - jacobian_a_.data;
 
-      // changeJacRefPoint(jacobian_b_, v2, J_v2);
-      // J_v2.data=J_v2.data - jacobian_b_.data;
+      changeJacRefPoint(jacobian_b_, v2, J_v2);
+      J_v2.data=J_v2.data - jacobian_b_.data;
 
-      // changeJacDotRefPoint(jacobian_a_,jacobian_dot_a_,robot_state->kdl_jnt_array_vel_, v1, J_v1_dot);
-      // J_v1_dot.data = J_v1_dot.data - jacobian_dot_a_.data;
+      changeJacDotRefPoint(jacobian_a_,jacobian_dot_a_,robot_state->kdl_jnt_array_vel_, v1, J_v1_dot);
+      J_v1_dot.data = J_v1_dot.data - jacobian_dot_a_.data;
 
-      // changeJacDotRefPoint(jacobian_b_,jacobian_dot_b_,robot_state->kdl_jnt_array_vel_, v2, J_v2_dot);
-      // J_v2_dot.data = J_v2_dot.data - jacobian_dot_b_.data;
+      changeJacDotRefPoint(jacobian_b_,jacobian_dot_b_,robot_state->kdl_jnt_array_vel_, v2, J_v2_dot);
+      J_v2_dot.data = J_v2_dot.data - jacobian_dot_b_.data;
       
-      // Eigen::VectorXd qdot=robot_state->kdl_jnt_array_vel_.qdot.data;
-      // Eigen::Vector3d v1_dot=J_v1.data.topRows<3>()*qdot;
-      // Eigen::Vector3d v2_dot=J_v2.data.topRows<3>()*qdot;
+      Eigen::VectorXd qdot=robot_state->kdl_jnt_array_vel_.qdot.data;
+      Eigen::Vector3d v1_dot=J_v1.data.topRows<3>()*qdot;
+      Eigen::Vector3d v2_dot=J_v2.data.topRows<3>()*qdot;
 
-      //return rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
+      return rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
       //CROSS PRODUCT END =========================================      
     }
 
@@ -80,35 +80,35 @@ namespace hiqp {
       KDL::Vector v2 = pose_b_.M * plane->getNormalKDL();
 
       //DOT PRODUCT =============================================      
-      return alignUnitVectors(v1, v2, robot_state);
+      //return alignUnitVectors(v1, v2, robot_state);
       //DOT PRODUCT END =========================================
 
       //CROSS PRODUCT =============================================      
-      // double q_nr=jacobian_a_.columns();
-      // KDL::Jacobian J_v1, J_p1, J_v2, J_v1_dot, J_v2_dot;
-      // J_v1.resize(q_nr);
-      // J_v2.resize(q_nr);
-      // J_p1.resize(q_nr);
-      // J_v1_dot.resize(q_nr);
-      // J_v2_dot.resize(q_nr);
+      double q_nr=jacobian_a_.columns();
+      KDL::Jacobian J_v1, J_p1, J_v2, J_v1_dot, J_v2_dot;
+      J_v1.resize(q_nr);
+      J_v2.resize(q_nr);
+      J_p1.resize(q_nr);
+      J_v1_dot.resize(q_nr);
+      J_v2_dot.resize(q_nr);
     
-      // changeJacRefPoint(jacobian_a_, v1, J_v1);
-      // J_v1.data=J_v1.data - jacobian_a_.data;
+      changeJacRefPoint(jacobian_a_, v1, J_v1);
+      J_v1.data=J_v1.data - jacobian_a_.data;
 
-      // changeJacRefPoint(jacobian_b_, v2, J_v2);
-      // J_v2.data=J_v2.data - jacobian_b_.data;
+      changeJacRefPoint(jacobian_b_, v2, J_v2);
+      J_v2.data=J_v2.data - jacobian_b_.data;
 
-      // changeJacDotRefPoint(jacobian_a_,jacobian_dot_a_,robot_state->kdl_jnt_array_vel_, v1, J_v1_dot);
-      // J_v1_dot.data = J_v1_dot.data - jacobian_dot_a_.data;
+      changeJacDotRefPoint(jacobian_a_,jacobian_dot_a_,robot_state->kdl_jnt_array_vel_, v1, J_v1_dot);
+      J_v1_dot.data = J_v1_dot.data - jacobian_dot_a_.data;
 
-      // changeJacDotRefPoint(jacobian_b_,jacobian_dot_b_,robot_state->kdl_jnt_array_vel_, v2, J_v2_dot);
-      // J_v2_dot.data = J_v2_dot.data - jacobian_dot_b_.data;
+      changeJacDotRefPoint(jacobian_b_,jacobian_dot_b_,robot_state->kdl_jnt_array_vel_, v2, J_v2_dot);
+      J_v2_dot.data = J_v2_dot.data - jacobian_dot_b_.data;
       
-      // Eigen::VectorXd qdot=robot_state->kdl_jnt_array_vel_.qdot.data;
-      // Eigen::Vector3d v1_dot=J_v1.data.topRows<3>()*qdot;
-      // Eigen::Vector3d v2_dot=J_v2.data.topRows<3>()*qdot;
+      Eigen::VectorXd qdot=robot_state->kdl_jnt_array_vel_.qdot.data;
+      Eigen::Vector3d v1_dot=J_v1.data.topRows<3>()*qdot;
+      Eigen::Vector3d v2_dot=J_v2.data.topRows<3>()*qdot;
       
-      //return rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
+      return rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
       //CROSS PRODUCT END =============================================
     }
 
@@ -166,11 +166,11 @@ namespace hiqp {
       J_v2_dot.data.topRows<3>() = vk_dot*(vk.transpose()*J_dp.data.topRows<3>()+dp.transpose()*J_vk.data.topRows<3>())+vk*(vk_dot.transpose()*J_dp.data.topRows<3>()+vk.transpose()*J_dp_dot.data.topRows<3>()+dp_dot.transpose()*J_vk.data.topRows<3>()+dp.transpose()*J_vk_dot.data.topRows<3>())-J_dp_dot.data.topRows<3>()+dp.dot(vk)*J_vk_dot.data.topRows<3>()+(dp_dot.dot(vk)+dp.dot(vk_dot))*J_vk.data.topRows<3>();
 
       //CROSS PRODUCT =============================================      
-      // return rotateVectors(v1, v2, v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
+       return rotateVectors(v1, v2, v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
       //CROSS PRODUCT END =========================================
 
       //DOT PRODUCT =============================================      
-      return alignUnitVectorVector(qdot,v1, v2, v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
+      //return alignUnitVectorVector(qdot,v1, v2, v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
       //DOT PRODUCT END =========================================
 
     }
@@ -215,11 +215,11 @@ namespace hiqp {
       Eigen::Vector3d v2_dot=J_v2.data.topRows<3>()*qdot;
 
       //CROSS PRODUCT ================================================
-      // return rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
+       return rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
       //CROSS PRODUCT END ============================================
 
       //DOT PRODUCT ================================================
-      return alignUnitVectorVector(qdot, Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
+       // return alignUnitVectorVector(qdot, Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
       //DOT PRODUCT END ============================================
  
     }
@@ -230,92 +230,62 @@ namespace hiqp {
 								      const RobotStatePtr robot_state) {
 
       //CROSS PRODUCT =============================================
-      // KDL::Vector v1 = pose_a_.M * frame1->getAxisXKDL();
-      // KDL::Vector v2 = pose_b_.M * frame2->getAxisXKDL();
+      KDL::Vector v1 = pose_a_.M * frame1->getAxisXKDL();
+      KDL::Vector v2 = pose_b_.M * frame2->getAxisXKDL();
 
-      // double q_nr=jacobian_a_.columns();
-      // KDL::Jacobian J_v1, J_p1, J_v2, J_v1_dot, J_v2_dot;
-      // J_v1.resize(q_nr);
-      // J_v2.resize(q_nr);
-      // J_p1.resize(q_nr);
-      // J_v1_dot.resize(q_nr);
-      // J_v2_dot.resize(q_nr);
+      double q_nr=jacobian_a_.columns();
+      KDL::Jacobian J_v1, J_p1, J_v2, J_v1_dot, J_v2_dot;
+      J_v1.resize(q_nr);
+      J_v2.resize(q_nr);
+      J_p1.resize(q_nr);
+      J_v1_dot.resize(q_nr);
+      J_v2_dot.resize(q_nr);
     
-      // changeJacRefPoint(jacobian_a_, v1, J_v1);
-      // J_v1.data=J_v1.data - jacobian_a_.data;
+      changeJacRefPoint(jacobian_a_, v1, J_v1);
+      J_v1.data=J_v1.data - jacobian_a_.data;
 
-      // changeJacRefPoint(jacobian_b_, v2, J_v2);
-      // J_v2.data=J_v2.data - jacobian_b_.data;
+      changeJacRefPoint(jacobian_b_, v2, J_v2);
+      J_v2.data=J_v2.data - jacobian_b_.data;
 
-      // changeJacDotRefPoint(jacobian_a_,jacobian_dot_a_,robot_state->kdl_jnt_array_vel_, v1, J_v1_dot);
-      // J_v1_dot.data = J_v1_dot.data - jacobian_dot_a_.data;
+      changeJacDotRefPoint(jacobian_a_,jacobian_dot_a_,robot_state->kdl_jnt_array_vel_, v1, J_v1_dot);
+      J_v1_dot.data = J_v1_dot.data - jacobian_dot_a_.data;
 
-      // changeJacDotRefPoint(jacobian_b_,jacobian_dot_b_,robot_state->kdl_jnt_array_vel_, v2, J_v2_dot);
-      // J_v2_dot.data = J_v2_dot.data - jacobian_dot_b_.data;
+      changeJacDotRefPoint(jacobian_b_,jacobian_dot_b_,robot_state->kdl_jnt_array_vel_, v2, J_v2_dot);
+      J_v2_dot.data = J_v2_dot.data - jacobian_dot_b_.data;
       
-      // Eigen::VectorXd qdot=robot_state->kdl_jnt_array_vel_.qdot.data;
-      // Eigen::Vector3d v1_dot=J_v1.data.topRows<3>()*qdot;
-      // Eigen::Vector3d v2_dot=J_v2.data.topRows<3>()*qdot;
+      Eigen::VectorXd qdot=robot_state->kdl_jnt_array_vel_.qdot.data;
+      Eigen::Vector3d v1_dot=J_v1.data.topRows<3>()*qdot;
+      Eigen::Vector3d v2_dot=J_v2.data.topRows<3>()*qdot;
       
-      // //abuse the rotateVectors function to compute the relevant quantities to align both axis
-      // rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
-
-      // //temporary save the task errors/jacobians
-      // Eigen::VectorXd ey=e_;
-      // Eigen::VectorXd ey_dot=e_dot_;  
-      // Eigen::MatrixXd Jy=J_;
-      // Eigen::MatrixXd Jy_dot=J_dot_;
-
-      // v1 = pose_a_.M * frame1->getAxisYKDL();
-      // v2 = frame2->getAxisYKDL();
-      
-      // changeJacRefPoint(jacobian_a_, v1, J_v1);
-      // J_v1.data=J_v1.data - jacobian_a_.data;
-
-      // changeJacRefPoint(jacobian_b_, v2, J_v2);
-      // J_v2.data=J_v2.data - jacobian_b_.data;
-
-      // changeJacDotRefPoint(jacobian_a_,jacobian_dot_a_,robot_state->kdl_jnt_array_vel_, v1, J_v1_dot);
-      // J_v1_dot.data = J_v1_dot.data - jacobian_dot_a_.data;
-
-      // changeJacDotRefPoint(jacobian_b_,jacobian_dot_b_,robot_state->kdl_jnt_array_vel_, v2, J_v2_dot);
-      // J_v2_dot.data = J_v2_dot.data - jacobian_dot_b_.data;
-      
-      // v1_dot=J_v1.data.topRows<3>()*qdot;
-      // v2_dot=J_v2.data.topRows<3>()*qdot;
-
-      // //overwrite the class member errors/jacobians
-      // rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
-
-      // //append the previously computed quantities
-      // e_.conservativeResize(2);
-      // e_(1)=ey(0);
-      // e_dot_.conservativeResize(2);
-      // e_dot_(1)=ey_dot(0);
-      // J_.conservativeResize(2,q_nr);
-      // J_.bottomRows<1>()=Jy;
-      // J_dot_.conservativeResize(2,q_nr);
-      // J_dot_.bottomRows<1>()=Jy_dot;
-      //CROSS PRODUCT END ==========================================
-
-      //DOT PRODUCT =============================================      
-      KDL::Vector ax1 = pose_a_.M * frame1->getAxisXKDL();
-      KDL::Vector ax2 = pose_b_.M * frame2->getAxisXKDL();
-      KDL::Vector ay1 = pose_a_.M * frame1->getAxisYKDL();
-      KDL::Vector ay2 = pose_b_.M * frame2->getAxisYKDL();
-
-      //abuse the alignUnitVectors function to compute the relevant quantities to align both axis
-      alignUnitVectors(ay1, ay2, robot_state);
+      //abuse the rotateVectors function to compute the relevant quantities to align both axis
+      rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
 
       //temporary save the task errors/jacobians
       Eigen::VectorXd ey=e_;
       Eigen::VectorXd ey_dot=e_dot_;  
       Eigen::MatrixXd Jy=J_;
-      Eigen::MatrixXd Jy_dot=J_dot_;  
-      double q_nr=J_.cols();
+      Eigen::MatrixXd Jy_dot=J_dot_;
+
+      v1 = pose_a_.M * frame1->getAxisYKDL();
+      v2 = frame2->getAxisYKDL();
+      
+      changeJacRefPoint(jacobian_a_, v1, J_v1);
+      J_v1.data=J_v1.data - jacobian_a_.data;
+
+      changeJacRefPoint(jacobian_b_, v2, J_v2);
+      J_v2.data=J_v2.data - jacobian_b_.data;
+
+      changeJacDotRefPoint(jacobian_a_,jacobian_dot_a_,robot_state->kdl_jnt_array_vel_, v1, J_v1_dot);
+      J_v1_dot.data = J_v1_dot.data - jacobian_dot_a_.data;
+
+      changeJacDotRefPoint(jacobian_b_,jacobian_dot_b_,robot_state->kdl_jnt_array_vel_, v2, J_v2_dot);
+      J_v2_dot.data = J_v2_dot.data - jacobian_dot_b_.data;
+      
+      v1_dot=J_v1.data.topRows<3>()*qdot;
+      v2_dot=J_v2.data.topRows<3>()*qdot;
 
       //overwrite the class member errors/jacobians
-      alignUnitVectors(ax1, ax2, robot_state);
+      rotateVectors(Eigen::Vector3d(v1(0),v1(1),v1(2)), Eigen::Vector3d(v2(0),v2(1),v2(2)), v1_dot, v2_dot, J_v1.data.topRows<3>(), J_v2.data.topRows<3>(), J_v1_dot.data.topRows<3>(), J_v2_dot.data.topRows<3>());
 
       //append the previously computed quantities
       e_.conservativeResize(2);
@@ -326,6 +296,36 @@ namespace hiqp {
       J_.bottomRows<1>()=Jy;
       J_dot_.conservativeResize(2,q_nr);
       J_dot_.bottomRows<1>()=Jy_dot;
+      //CROSS PRODUCT END ==========================================
+
+      //DOT PRODUCT =============================================      
+      // KDL::Vector ax1 = pose_a_.M * frame1->getAxisXKDL();
+      // KDL::Vector ax2 = pose_b_.M * frame2->getAxisXKDL();
+      // KDL::Vector ay1 = pose_a_.M * frame1->getAxisYKDL();
+      // KDL::Vector ay2 = pose_b_.M * frame2->getAxisYKDL();
+
+      // //abuse the alignUnitVectors function to compute the relevant quantities to align both axis
+      // alignUnitVectors(ay1, ay2, robot_state);
+
+      // //temporary save the task errors/jacobians
+      // Eigen::VectorXd ey=e_;
+      // Eigen::VectorXd ey_dot=e_dot_;  
+      // Eigen::MatrixXd Jy=J_;
+      // Eigen::MatrixXd Jy_dot=J_dot_;  
+      // double q_nr=J_.cols();
+
+      // //overwrite the class member errors/jacobians
+      // alignUnitVectors(ax1, ax2, robot_state);
+
+      // //append the previously computed quantities
+      // e_.conservativeResize(2);
+      // e_(1)=ey(0);
+      // e_dot_.conservativeResize(2);
+      // e_dot_(1)=ey_dot(0);
+      // J_.conservativeResize(2,q_nr);
+      // J_.bottomRows<1>()=Jy;
+      // J_dot_.conservativeResize(2,q_nr);
+      // J_dot_.bottomRows<1>()=Jy_dot;
       //DOT PRODUCT END =============================================      
       return 0;
     }
